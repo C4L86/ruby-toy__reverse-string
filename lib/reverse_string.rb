8b9b6b9082c7d80a31db74ab6f79df1ae9ab0b8a
Chris Foral <chris.foral@gmail.com>
2016-01-08 21:28:06 +0000

    Done

--- lib/reverse_string.rb
+++ lib/reverse_string.rb
@@ -1,5 +1,23 @@
 def reverse_characters(str)
+    str_ary = str.split(//)
+    answer_ary = []
+    
+    while str_ary.length > 0
+        answer_ary.push(str_ary[-1])
+        str_ary.pop
+    end
+
+answer_ary.join
 end
 
 def reverse_words(str)
+    str_ary = str.split
+    answer_ary = []
+    
+    while str_ary.length > 0
+        answer_ary.push(str_ary[-1])
+        str_ary.pop
+    end
+
+answer_ary.join(' ')
 end
